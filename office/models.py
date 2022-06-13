6a0c9a5c72d09b0ea80d925f697d855d35f07eb0
Moses Burkhart <moses.burkhart1@gmail.com>
2022-06-13 19:57:18 -0400

    added validators to model

--- office/models.py
+++ office/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
-
 class Patient(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    age = models.IntegerField()
+    age = models.IntegerField(validators=[MaxValueValidator(120), MinValueValidator(0)])
+    heart_rate = models.IntegerField(default=60,validators=[MaxValueValidator(300),MinValueValidator(1)])
 
     def __str__(self):
 
